c75699226599a1fc2732d2f7a553c994eb43e251
greatkene <ihevuemeg@gmail.com>
2023-04-10 19:21:13 +0100

    chores

--- app/screens/ListingScreen.jsx
+++ app/screens/ListingScreen.jsx
@@ -1,11 +1,24 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { withAuthenticator } from "aws-amplify-react-native";
+import { Auth } from "aws-amplify";
 
 const ListingScreen = () => {
+  Auth.currentAuthenticatedUser()
+    .then((user) => {
+      console.log(user.attributes.email);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
   return (
     <SafeAreaView>
-      <Text>Listing</Text>
+      <View>
+        <Text>Images</Text>
+        <Text>Category</Text>
+      </View>
     </SafeAreaView>
   );
 };
